f32b43f9bf7d5101bc09708706fe836bd2d23419
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-01-29 13:14:49 -0500

    Chapter 7

--- Chapter07/basic_exception.php
+++ Chapter07/basic_exception.php
@@ -1,11 +1,14 @@
-<?php
-
-try  {
-  throw new Exception("A terrible error has occurred", 42);
-}
-catch (Exception $e) {
-  echo "Exception ". $e->getCode(). ": ". $e->getMessage()."<br />".
-  " in ". $e->getFile(). " on line ". $e->getLine(). "<br />";
-}
-
+<?php 
+    try{
+        throw new Exception("Un terrible error", 42);
+    } catch(Exception $e) {
+        echo "Expection: ".$e->getCode().": ",$e->getMessage()."<br />".
+            " in ".$e->getFile(). " on line ".$e->getLine(). "<br />";
+        echo $e;                                         
+    }
 ?>
+<pre>
+    <?php 
+        var_dump($e);
+    ?>
+</pre>
---
+++ Chapter07/home.php
@@ -0,0 +1,9 @@
+<?php 
+    try{
+        //hacer algo
+    } catch (Exception $e) {
+        //Controlar la Expecion
+    } finally {
+        echo 'Joder esto es PHP!';
+    }
+?>
