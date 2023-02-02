16cba44faff78783d40379dfcb84ab2758c742c9
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-02-01 19:16:20 -0500

    Connect MYSQL and PHP

--- Chapter11/results.php
+++ Chapter11/results.php
@@ -1,59 +1,68 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-  <title>Book-O-Rama Search Results</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book-O-Rama Catalog Search</title>
 </head>
 <body>
-  <h1>Book-O-Rama Search Results</h1>
-  <?php
-    // create short variable names
-    $searchtype=$_POST['searchtype'];
-    $searchterm="%{$_POST['searchterm']}%";
+    <h1>Book-O-Rama Catalog Search</h1>
+    <?php 
+        //Crear nombres de variables
+        $searchtype = $_POST['searchtype'];
+        $searchterm = trim($_POST['searchterm']);
 
-    if (!$searchtype || !$searchterm) {
-       echo '<p>You have not entered search details.<br/>
-       Please go back and try again.</p>';
-       exit;
-    }
+        if(!$searchtype || !$searchterm){
+            echo '<p>You have not entered search details. <br>
+                Please go back and try again.
+            </p>';
+            exit;
+        }
 
-    // whitelist the searchtype
-    switch ($searchtype) {
-      case 'Title':
-      case 'Author':
-      case 'ISBN':   
-        break;
-      default: 
-        echo '<p>That is not a valid search type. <br/>
-        Please go back and try again.</p>';
-        exit; 
-    }
+        // Añadir el tipo de búsqueda a la lista blanca
+        switch($searchtype) {
+            case 'Title':
+            case 'Author':
+            case 'ISBN':
+                break;
+            default:
+                echo '<p>That is not a valid search type. <br>
+                    Please go back and try again.
+                </p>';
+                exit;
+        }
 
-    $db = new mysqli('localhost', 'bookorama', 'bookorama123', 'books');
-    if (mysqli_connect_errno()) {
-       echo '<p>Error: Could not connect to database.<br/>
-       Please try again later.</p>';
-       exit;
-    }
+        //Conectar con la Base de Datos
+        $db = new mysqli('localhost', 'root', '123456', 'books');
+        if(mysqli_connect_errno()){
+            echo '<p>Error: Could not connect to database. <br>
+                Please try again later.
+            </p>';
+            exit;
+        }
 
-    $query = "SELECT ISBN, Author, Title, Price FROM Books WHERE $searchtype = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param('s', $searchterm);  
-    $stmt->execute();
-    $stmt->store_result();
-  
-    $stmt->bind_result($isbn, $author, $title, $price);
+        //Consultar la Base de Datos
+        $query = "SELECT ISBN, Author, Title, Price FROM books WHERE $searchtype = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param('s', $searchterm);
+        $stmt->execute();
 
-    echo "<p>Number of books found: ".$stmt->num_rows."</p>";
+        //Recuperar Resultados de la consulta
+        $stmt->store_result();
+        $stmt->bind_result($isbn, $author, $title, $price);
 
-    while($stmt->fetch()) {
-      echo "<p><strong>Title: ".$title."</strong>";
-      echo "<br />Author: ".$author;
-      echo "<br />ISBN: ".$isbn;
-      echo "<br />Price: \$".number_format($price,2)."</p>";
-    }
+        echo "<p>Number of books found: ".$stmt->num_rows."</p>";
 
-    $stmt->free_result();
-    $db->close();
-  ?>
+        while($stmt->fetch()){
+            echo "<p><strong>Title: ".$title."</strong>";
+            echo "<br> Author: ".$author;
+            echo "<br> ISBN: ".$isbn;
+            echo "<br> Price: \$".number_format($price,2)."</p>";
+        }
+
+        $stmt->free_result();
+        $db->close();
+    ?>
 </body>
 </html>
--- Chapter11/results_pdo.php
+++ Chapter11/results_pdo.php
@@ -1,69 +1,73 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-  <title>Book-O-Rama Search Results</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book-O-Rama Catalog Search</title>
 </head>
 <body>
-  <h1>Book-O-Rama Search Results</h1>
-  <?php
-    // create short variable names
-    $searchtype=$_POST['searchtype'];
-    $searchterm="%{$_POST['searchterm']}%";
+    <h1>Book-O-Rama Catalog Search</h1>
+    <?php 
+        //crear nombres de variables cortos
+        $searchtype = $_POST['searchtype'];
+        $searchterm = trim($_POST['searchterm']);
 
-    if (!$searchtype || !$searchterm) {
-       echo '<p>You have not entered search details.<br/>
-       Please go back and try again.</p>';
-       exit;
-    }
+        if(!$searchtype || !$searchterm){
+            echo '<p>You have not entered search details. <br>
+                Please go back and try again.
+            </p>';
+            exit;
+        }
 
-    // whitelist the searchtype
-    switch ($searchtype) {
-      case 'Title':
-      case 'Author':
-      case 'ISBN':   
-        break;
-      default: 
-        echo '<p>That is not a valid search type. <br/>
-        Please go back and try again.</p>';
-        exit; 
-    }
+        // Añadir el tipo de búsqueda a la lista blanca
+        switch($searchtype) {
+            case 'Title':
+            case 'Author':
+            case 'ISBN':
+                break;
+            default:
+                echo '<p>That is not a valid search type. <br>
+                    Please go back and try again.
+                </p>';
+                exit;
+        }
 
-    // set up for using PDO
-    $user = 'bookorama';
-    $pass = 'bookorama123';
-    $host = 'localhost';
-    $db_name = 'books';
+        //Configurar para utilizar PDO
+        $user = 'root';
+        $pass = '123456';
+        $host = 'localhost';
+        $db_name = 'books';
 
-    // set up DSN
-    $dsn = "mysql:host=$host;dbname=$db_name";
+        // Configurar DSN
+        $dsn = "mysql:host=$host;dbname=$db_name";
 
-    // connect to database
-    try {
-      $db = new PDO($dsn, $user, $pass); 
+        //Conectar a la base de datos
+        try {
+            $db = new PDO($dsn, $user, $pass);
 
-      // perform query
-      $query = "SELECT ISBN, Author, Title, Price FROM Books WHERE $searchtype like :searchterm";  
-      $stmt = $db->prepare($query);  
-      $stmt->bindParam(':searchterm', $searchterm);
-      $stmt->execute(); 
+            //Ejecutar Consulta
+            $query = "SELECT ISBN, Author, Title, Price FROM books WHERE $searchtype = :searchterm";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':searchterm', $searchterm);
+            $stmt->execute();
 
-      // get number of returned rows  
-      echo "<p>Number of books found: ".$stmt->rowCount()."</p>"; 
+            //Obtener el numero de filas devueltas
+            echo "<p>Number of books found: ".$stmt->rowCount()."</p>";
 
-      // display each returned row
-      while($result = $stmt->fetch(PDO::FETCH_OBJ)) {                                                       
-        echo "<p><strong>Title: ".$result->Title."</strong>";                               
-        echo "<br />Author: ".$result->Author;                                              
-        echo "<br />ISBN: ".$result->ISBN;                                                  
-        echo "<br />Price: \$".number_format($result->Price, 2)."</p>";                                         
-      }         
+            //Mostrart cada filda devuleta
+            while($result = $stmt->fetch(PDO::FETCH_OBJ)){
+                echo "<p><strong>Title: ".$result->Title."</strong>";
+                echo "<br> Author: ".$result->Author;
+                echo "<br> ISBN: ".$result->ISBN;
+                echo "<br> Price: \$".number_format($result->Price, 2)."</p>";
+            }
+            $db = NULL;
+        } catch (PDOException $e) {
+            echo "Error: ".$e->getMessage();
+            exit;
+        }
+    ?>
 
-      // disconnect from database
-      $db = NULL;
-    } catch (PDOException $e) {
-      echo "Error: ".$e->getMessage();
-      exit;
-    }
-  ?>
 </body>
 </html>
